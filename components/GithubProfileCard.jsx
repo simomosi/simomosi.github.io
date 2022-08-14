b92503b4737267ef477649cc03fe700410da0d8d
simomosi <37104045+simomosi@users.noreply.github.com>
2022-08-14 09:13:05 +0100

    Removed location from github info

--- components/GithubProfileCard.jsx
+++ components/GithubProfileCard.jsx
@@ -26,10 +26,10 @@ const GithubProfileCard = ({ prof }) => {
 								INBOX IS OPEN FOR ALL
 							</p>
 							<p className="text-white mt-3">{prof.bio}</p>
-							<div className="my-3 icon-shape bg-gradient-white shadow rounded text-info">
+							{/* <div className="my-3 icon-shape bg-gradient-white shadow rounded text-info">
 								<i className="ni ni-pin-3 text-info mr-2" />
 								{prof.location}
-							</div>
+							</div> */}
 							<SocialLinks />
 						</Col>
 					</Row>
